a122b135ad631f1bcf16fc15d75981a4645be342
Carlos Hansen <hansen.mendoza@gmail.com>
2019-12-30 00:28:31 -0500

    Read from SD as function
    Co-authored-by: Tenna Boeriis Rasmussen <au523844@uni.au.dk>

--- MiniProject/MiniProject.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_lab4.elf_on_local.tcl
+++ MiniProject/MiniProject.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_lab4.elf_on_local.tcl
@@ -1,7 +1,7 @@
 connect -url tcp:127.0.0.1:3121
-source C:/ERTS_miniproject/E19_Embedded/MiniProject/MiniProject.sdk/system_wrapper_hw_platform_0/ps7_init.tcl
+source C:/master/EmbeddedPRJ/MiniProject/MiniProject.sdk/system_wrapper_hw_platform_0/ps7_init.tcl
 targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zybo 210279A6A756A"} -index 0
-loadhw -hw C:/ERTS_miniproject/E19_Embedded/MiniProject/MiniProject.sdk/system_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
+loadhw -hw C:/master/EmbeddedPRJ/MiniProject/MiniProject.sdk/system_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
 configparams force-mem-access 1
 targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zybo 210279A6A756A"} -index 0
 stop
@@ -10,7 +10,7 @@ ps7_post_config
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo 210279A6A756A"} -index 0
 rst -processor
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo 210279A6A756A"} -index 0
-dow C:/ERTS_miniproject/E19_Embedded/MiniProject/MiniProject.sdk/lab4/Debug/lab4.elf
+dow C:/master/EmbeddedPRJ/MiniProject/MiniProject.sdk/lab4/Debug/lab4.elf
 configparams force-mem-access 0
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo 210279A6A756A"} -index 0
 con
